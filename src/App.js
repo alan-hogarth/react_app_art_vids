4f3e69e50658e10bf868583fecdf37f6ec73141f
Alan Hogarth <alan.hogarth@outlook.com>
2020-12-28 14:12:49 +0000

    first commit: API loaded

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  
+  const fetchArticles = (()=>{
+      const url=`https://content.guardianapis.com/search?q=literature&format=json&api-key=test`
+
+      fetch(url)
+      .then((res)=>res.json())
+      .then((data)=>{
+        setArticles(data.response)
+      })
+
+  })
+
+  useEffect(()=>{
+    fetchArticles();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>hello
+ 
+   </>
   );
 }
 
